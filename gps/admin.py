e6111244be3c7a846e0dda9caf6d3efb41b3af55
panagiotispapazisis <panagiotis60@outlook.com>
2024-11-26 14:51:14 +0200

    a

--- gps/admin.py
+++ gps/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import GpsData
 # Register your models here.
+
+@admin.register(GpsData)
+class GpsDataAdmin(admin.ModelAdmin):
+    list_display = ['message', 'timestamp']
+    
